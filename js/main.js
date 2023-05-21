a42ee24a0b04e620453cd36788e524b451a5656c
Oleg Starichenko <staro.oleg@gmail.com>
2023-05-21 23:16:12 +0300

    add sections follow, join, footer

--- js/main.js
+++ js/main.js
@@ -1,58 +1,76 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-	navToggle = document.getElementById('nav-toggle'),
-	navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /* Menu Show */
 /* Validate if constant exist */
 if (navToggle) {
-	navToggle.addEventListener('click', () => {
-		navMenu.classList.add('show-menu')
-	})
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
 }
 
 /* Menu Hidden */
 /* Validate if constant exist */
 if (navClose) {
-	navClose.addEventListener('click', () => {
-		navMenu.classList.remove('show-menu')
-	})
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () => {
-	const navMenu = document.getElementById('nav-menu')
+    const navMenu = document.getElementById('nav-menu')
 
-	// When we click on each nav__link, we remove tje show-menu class
-	navMenu.classList.remove('show-menu')
+    // When we click on each nav__link, we remove tje show-menu class
+    navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
 const blurHeader = () => {
-	const nav = document.getElementById('header')
+    const nav = document.getElementById('header')
 
-	/* if (this.scrollY >= 80) {
+    /* if (this.scrollY >= 80) {
 		nav.classList.add('blur-header')
 	} else {
 		nav.classList.remove('blur-header')
 	} */
 
-	this.scrollY >= 50
-		? nav.classList.add('blur-header')
-		: nav.classList.remove('blur-header')
+    this.scrollY >= 50 ? nav.classList.add('blur-header') : nav.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
 
+    this.scrollY >= 500 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight
+            ? sectionClass.classList.add('active-link')
+            : sectionClass.classList.remove('active-link')
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
